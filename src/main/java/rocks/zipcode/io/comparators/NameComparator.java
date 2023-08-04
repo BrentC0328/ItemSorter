9a2e8ea1617ee2116e70b699ed309a91ba5779a6
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-04 16:32:59 -0400

    Item and comparators created

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,19 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+
+
+    @Override
+    public int compare(Item o1, Item o2) {
+        String name1 = o1.getName();
+        String name2 = o2.getName();
+        return name1.compareTo(name2);
+    }
 }
--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,20 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+
+
+    @Override
+    public int compare(Item o1, Item o2) {
+        Double price1 = o1.getPrice();
+        Double price2 = o2.getPrice();
+        
+        return price1.compareTo(price2);
+    }
 }
